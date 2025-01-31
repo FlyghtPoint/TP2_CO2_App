ef1aa8607ac8b2c3a4c1ede740146a32b324f83b
Victor LEFÈVRE <victor.lefevre@etu.unicaen.fr>
2025-01-31 14:32:40 +0100

    Initial commit

---
+++ mlmodel.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import sklearn as skl
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+consumption_df = pd.read_csv('./FuelConsumption.csv')
+# print(consumption_df.head())
+# consumption_df.info()
+le=LabelEncoder()
+# print(consumption_df['MAKE'].unique())
+# print(consumption_df['MODEL'].unique())
+# print(consumption_df['VEHICLECLASS'].unique())
+# print(consumption_df['TRANSMISSION'].unique())
+# print(consumption_df['FUELTYPE'].unique())
+consumption_df['MAKE']=le.fit_transform(consumption_df['MAKE'])
+consumption_df['MODEL']=le.fit_transform(consumption_df['MODEL'])
+consumption_df['VEHICLECLASS']=le.fit_transform(consumption_df['VEHICLECLASS'])
+consumption_df['TRANSMISSION']=le.fit_transform(consumption_df['TRANSMISSION'])
+consumption_df['FUELTYPE']=le.fit_transform(consumption_df['FUELTYPE'])
+
+x = consumption_df[['MODELYEAR', 'ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB']]
+y = consumption_df['CO2EMISSIONS']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, shuffle=False, random_state=0)
+
+poly_features = PolynomialFeatures(degree=3, include_bias=False)
+std_scaler = StandardScaler()
+lin_reg = LinearRegression()
+polynomial_regression = make_pipeline(poly_features, std_scaler, lin_reg)
+polynomial_regression.fit(x_train, y_train)
+print('Correlation Train =', polynomial_regression.score(x_train, y_train))
+print('Correlation Test =', polynomial_regression.score(x_test, y_test))
+
+predictions_test = polynomial_regression.predict(x_test)
+
+import pickle
+
+filename = 'model.pickle'
+pickle.dump(polynomial_regression, open(filename, 'wb'))
